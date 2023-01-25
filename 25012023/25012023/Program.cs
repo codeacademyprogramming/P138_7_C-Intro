ec639f667c99f9c4e0626146efda235b47b62237
eaghayev <eaghayevv@gmail.com>
2023-01-25 12:57:05 +0400

    project added

---
+++ 25012023/25012023/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _25012023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int loopCount = 0;
+            int num = 7919;
+
+            bool isPrime = true;
+            for(var i=2; i*i<=num; i++)
+            {
+                loopCount++;
+
+                if (num % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if(isPrime == true)
+                Console.WriteLine("Sadedir");
+            else
+                Console.WriteLine("Murekkebdir");
+
+
+            Console.WriteLine(loopCount);
+        }
+    }
+}
---
+++ FirstApp/FirstApp/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace FirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string surname;
+            int num1;
+
+            surname = "Abbasov";
+            num1 = 34;
+            num1 = 55;
+            num1 = 88;
+
+            string name = "Hikmet";
+            char letter = 'A';
+
+            bool check = false;
+
+            byte age = 255;
+
+            var num = 55;
+
+            float price = 45.5f;
+
+
+            var avgPoint = 55;
+
+            var monthlyCreditPercent = 66.5; //camel case
+
+            var MonthlyCreditPercent = 66.5; // pascal case
+
+            var monthly_credit_percent = 66.5; //snake case
+
+            // var monthly-credit-percent  kebab case
+
+            bool isPasswordCorret = false;
+
+            if(!isPasswordCorret)
+            {
+                Console.WriteLine("Sifre yanlisdir");
+            }
+           
+
+            int number = 12;
+
+            if (number > 0)
+            {
+                if (number%2==0)
+                {
+                    Console.WriteLine("musbet ve cutdur");
+                }
+                else
+                {
+                    Console.WriteLine("musbet ve tekdir");
+                }
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("menfidir");
+            }
+            else
+            {
+                Console.WriteLine("Sifirdir");
+            }
+
+            
+
+
+
+
+        }
+    }
+}
